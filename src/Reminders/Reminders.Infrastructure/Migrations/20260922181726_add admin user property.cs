6ed941d7c01c2d365d8aec3d31f58b0d9f45de8d
arsbelkin <arseniibelkin@gmail.com>
2026-09-23 11:49:04 +0300

    add migrations files

---
+++ src/Reminders/Reminders.Infrastructure/Migrations/20260922181726_add admin user property.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reminders.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addadminuserproperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                comment: "является ли пользователь админом");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "Users");
+        }
+    }
+}
