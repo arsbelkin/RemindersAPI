d1e0c602bc276f800d55badab5bd9a0c11040be8
arsbelkin <arseniibelkin@gmail.com>
2026-09-23 12:45:45 +0300

    add delay worker

--- src/Reminders/Reminders.DbScannerWorker/Worker.cs
+++ src/Reminders/Reminders.DbScannerWorker/Worker.cs
@@ -18,21 +18,55 @@ public class Worker : BackgroundService
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        await DoWorkAsync(stoppingToken);
+        
         while (!stoppingToken.IsCancellationRequested)
         {
-            using var scope = _scopeFactory.CreateScope();
+            await Task.Delay(CalculateDelay(), stoppingToken);
             
-            var userRep =  scope.ServiceProvider.GetRequiredService<IUserRepository>();
-
-            var user = await userRep.GetUserByUsernameOrEmailAsync("arseniibelkin@gmail.com");
-            
-            if (_logger.IsEnabled(LogLevel.Information))
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                _logger.LogInformation($"User: {user?.Email}");
-            }
-
-            await Task.Delay(10_000, stoppingToken);
+            await DoWorkAsync(stoppingToken);
         }
+    }
+
+    private async Task DoWorkAsync(CancellationToken stoppingToken)
+    {
+        using var scope = _scopeFactory.CreateScope();
+            
+        var userRep =  scope.ServiceProvider.GetRequiredService<IUserRepository>();
+
+        var user = await userRep.GetUserByUsernameOrEmailAsync("arseniibelkin@gmail.com");
+            
+        if (_logger.IsEnabled(LogLevel.Information))
+        {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            _logger.LogInformation($"User: {user?.Email}");
+        }
+
+        await Task.CompletedTask;
+    }
+
+    private static TimeSpan CalculateDelay()
+    {
+        var now = DateTime.Now;
+        
+        // var nextHour = new DateTime(
+        //     now.Year,
+        //     now.Month,
+        //     now.Day,
+        //     now.Hour,
+        //     0,
+        //     0
+        // ).AddHours(1);
+        
+        var nextHour = new DateTime(
+            now.Year,
+            now.Month,
+            now.Day,
+            now.Hour,
+            now.Minute,
+            now.Second
+        ).AddSeconds(10);
+
+        return nextHour - now;
     }
 }
