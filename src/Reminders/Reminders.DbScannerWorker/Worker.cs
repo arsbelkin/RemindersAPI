37d5b0210a2f7f99458c97026d424435e89bfac5
arsbelkin <arseniibelkin@gmail.com>
2026-09-23 12:23:20 +0300

    test worker

--- src/Reminders/Reminders.DbScannerWorker/Worker.cs
+++ src/Reminders/Reminders.DbScannerWorker/Worker.cs
@@ -1,17 +1,38 @@
+using Reminders.Application.Repositories.Interfaces;
+
 namespace Reminders.DbScannerWorker;
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
+public class Worker : BackgroundService
 {
+    private readonly IServiceScopeFactory _scopeFactory;
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(
+        IServiceScopeFactory scopeFactory, 
+        ILogger<Worker> logger
+        )
+    {
+        _scopeFactory = scopeFactory;
+        _logger = logger;
+    }
+    
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (logger.IsEnabled(LogLevel.Information))
+            using var scope = _scopeFactory.CreateScope();
+            
+            var userRep =  scope.ServiceProvider.GetRequiredService<IUserRepository>();
+
+            var user = await userRep.GetUserByUsernameOrEmailAsync("arseniibelkin@gmail.com");
+            
+            if (_logger.IsEnabled(LogLevel.Information))
             {
-                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation($"User: {user?.Email}");
             }
 
-            await Task.Delay(10000, stoppingToken);
+            await Task.Delay(10_000, stoppingToken);
         }
     }
 }
